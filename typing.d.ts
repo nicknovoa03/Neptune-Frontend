6c3db648037033527dd64700a113857326bfd923
Nick Novoa <nicknovoa03@gmail.com>
2024-07-23 15:19:43 -0500

    add typings

--- typing.d.ts
+++ typing.d.ts
@@ -1,10 +1,31 @@
-interface Message {
-    text: string;
-  
-    createdAt: admin.firestore.Timestamp;
-    user: {
-      name: string;
-      email: string;
-      avatar: string;
-    };
+type Message = {
+  text: string;
+
+  createdAt: admin.firestore.Timestamp;
+  user: {
+    name: string;
+    email: string;
+    avatar: string;
+  };
+}
+
+export type CSVRecord = {
+  source: string;
+  target: string;
+  association: string;
+}
+
+export type Node = {
+  id: string;
+}
+
+export type Link = {
+  source: string;
+  target: string;
+  association: string;
+}
+
+export type GraphData = {
+  nodes: Node[];
+  links: Link[];
 }
