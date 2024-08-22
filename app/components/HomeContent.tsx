960298d961ca070dd054a35d50d370a815ad6884
Nick Novoa <nicknovoa03@gmail.com>
2024-08-22 11:04:37 -0500

    feature: add logo to home content

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import logo from '@/app/public/NeptuneLogo.png'
+
 const HomeContent = () => {
   return (
     <div className="flex flex-col items-center justify-between py-4 max-w-screen min-h-[90vh] overflow-y-hidden ">
       <div className="mt-80">
-        <h1 className="text-[#565869] text-4xl font-bold">NeptuneGPT</h1>
+        <Image className='opacity-50 grayscale'
+          src={logo} // Update this path to the actual path of your logo
+          alt="NeptuneGPT Logo"
+          width={300} // Adjust the width as needed
+          height={400} // Adjust the height as needed
+        />
       </div>
     </div>
   )
