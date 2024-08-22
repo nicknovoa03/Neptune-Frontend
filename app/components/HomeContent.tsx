e5ce875a43eeb8de051ec7e695151ee2b067f8d6
Nick Novoa <nicknovoa03@gmail.com>
2024-08-22 17:30:19 -0500

    refactor: change opacity of logo

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -5,7 +5,7 @@ const HomeContent = () => {
   return (
     <div className="flex flex-col items-center justify-between py-4 max-w-screen min-h-[90vh] overflow-y-hidden ">
       <div className="mt-80">
-        <Image className='opacity-50 grayscale'
+        <Image className='opacity-60 grayscale'
           src={logo} // Update this path to the actual path of your logo
           alt="NeptuneGPT Logo"
           width={300} // Adjust the width as needed
